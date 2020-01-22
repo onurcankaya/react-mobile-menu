63193f4019ab4822fceb5412f1ce12aff17df3ef
onurcankaya <onur.eren.cankaya@gmail.com>
2020-01-22 14:58:54 +0100

    added sample data

---
+++ src/menu-data.js
@@ -0,0 +1,18 @@
+export const menuData = [
+  {
+    label: 'Home',
+    url: '/',
+  },
+  {
+    label: 'About',
+    url: '/about',
+  },
+  {
+    label: 'Contact',
+    url: '/contact',
+  },
+  {
+    label: 'Blog',
+    url: '/blog',
+  },
+]
