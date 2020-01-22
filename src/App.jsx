1156e7ae6ca148786757778ae88bbe0e9b578915
onurcankaya <onur.eren.cankaya@gmail.com>
2020-01-22 11:43:00 +0100

    added font awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function App() {
-  return <div>React Mobile Menu</div>
+  return (
+    <div>
+      <i className="fas fa-bars"></i>
+    </div>
+  )
 }
 
 export default App
